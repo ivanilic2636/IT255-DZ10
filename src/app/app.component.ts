a3241e248f0782e6b0b415942179a5b503fc7b9a
ivanilic2636 <ivan.ilic028@gmail.com>
2017-05-25 02:49:16 +0200

    IT255-DZ10-Ivan Ilic 2636

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './template.html',
+
 })
 export class AppComponent {
-  title = 'app works!';
+
+  private vozaci = 'http://localhost/angular/getDriver.php';
+  data: Object[];
+  name: String = "";
+  constructor(private http: Http){
+    this.http.get(this.vozaci).subscribe(
+      data=>{
+        this.data = JSON.parse(data["_body"]);
+      },
+      err=>console.log(err.text()),
+    ()=>{
+
+    }
+  );
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+
+
+
+
+import { SearchPipe } from './pipes/search';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+    SearchPipe
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    HttpModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
